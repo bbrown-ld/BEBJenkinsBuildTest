3a33f1a832fa6a86e43fa8db9f189f9b0890892b
bbrown <bbrown@livingdirect.com>
2016-06-15 10:30:18 -0500

    Initial creation and upload of project

---
+++ JenkinsBuildTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JenkinsBuildTestLib;
+
+namespace JenkinsBuildTest
+{
+    class Program
+    {
+        /// <summary>Main entry-point for this application.</summary>
+        ///
+        /// <remarks>BBrown, 6/15/2016.</remarks>
+        ///
+        /// <param name="args">Array of command-line argument strings.</param>
+
+        static void Main(string[] args)
+        {
+
+            var s1 = "bbrown127@gmail.com";
+            var s2 = "bbrown123";
+            
+            Console.WriteLine("{0} is a valid email: {1}", s1, s1.IsValidEmailAddress());
+            Console.WriteLine("{0} is a valid email: {1}", s2, s2.IsValidEmailAddress());
+
+        }
+    }
+}
---
+++ JenkinsBuildTestLibTests/StringExtTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JenkinsBuildTestLib;
+using NUnit.Framework;
+namespace JenkinsBuildTestLib.Tests
+{
+    [TestFixture()]
+    public class StringExtTests
+    {
+        [Test()]
+        public void IsValidEmailAddressTest_ValidEmailAddressSupplied_IsValidEmail()
+        {
+
+            var s = "bbrown@livingdirect.com";
+
+            var actual = s.IsValidEmailAddress();
+
+            Assert.IsTrue(actual);
+        }
+
+
+        [Test()]
+        public void IsValidEmailAddressTest_InValidEmailAddressSupplied_IsNotValidEmail()
+        {
+
+            var s = "bbrown127";
+
+            var actual = s.IsValidEmailAddress();
+
+            Assert.IsFalse(actual);
+        }
+
+
+    }
+}
